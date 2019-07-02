b9f18c8fe08f59ab26e47c02fa8f4892358b8d29
hanmeng221 <hanmeng221@github.com>
2019-07-02 03:19:40 +0000

    change

---
+++ vim_complete.sh
@@ -0,0 +1,1 @@
+cd ~/.vim/plugged/YouCompleteMe && python3 install.py --go-completer
--- vim_init.sh
+++ vim_init.sh
@@ -3,13 +3,11 @@ sudo tar xzvf go1.12.linux-amd64.tar.gz -C /usr/local/
 mkdir -p $HOME/go/{bin,pkg,src}
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo apt install build-essential cmake python3-dev
+sudo apt install build-essential cmake python3-dev -y
 sudo echo `export GOPATH=$HOME/go` > /etc/profile.d/go.sh
 sudo echo `export GOROOT=/usr/local/go` > /etc/profile.d/go.sh
 sudo echo `export PATH=$PATH:$GOROOT/bin:$GOPATH/bin` > /etc/profile.d/go.sh
-sudo cp ./.vimrc $HOME
-sudo mkdir -p ~/.vim/plugged
-sudo vi -c "PlugInstall"
-sudo vi -c "GoInstallBinaries"
-cd ~/.vim/plugged/YouCompleteMe && python3 install.py --go-completer 
-
+cp ./.vimrc $HOME
+mkdir -p ~/.vim/plugged
+rm go1.12.linux-amd64.tar.gz
+echo "please run :PlugInstall and :GoInstallBinaries in vim and run vim_complete.sh"
