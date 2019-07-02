fe2c3bdcf7dfacaa24e252f03d07283422209f91
hanmeng221 <hanmeng221@github.com>
2019-07-02 04:21:22 +0000

    update

--- vim_init.sh
+++ vim_init.sh
@@ -1,13 +1,11 @@
-wget https://dl.google.com/go/go1.12.linux-amd64.tar.gz
-sudo tar xzvf go1.12.linux-amd64.tar.gz -C /usr/local/
-mkdir -p $HOME/go/{bin,pkg,src}
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+sudo -u ubuntu wget https://dl.google.com/go/go1.12.linux-amd64.tar.gz
+tar xzvf go1.12.linux-amd64.tar.gz -C /usr/local/
+sudo -u ubuntu mkdir -p $HOME/go/{bin,pkg,src}
+sudo -u ubuntu mkdir -m 775 ~/.vim
+sudo -u ubuntu curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo apt install build-essential cmake python3-dev -y
-sudo echo `export GOPATH=$HOME/go` > /etc/profile.d/go.sh
-sudo echo `export GOROOT=/usr/local/go` > /etc/profile.d/go.sh
-sudo echo `export PATH=$PATH:$GOROOT/bin:$GOPATH/bin` > /etc/profile.d/go.sh
-cp ./.vimrc $HOME
-mkdir -p ~/.vim/plugged
+apt install build-essential cmake python3-dev -y
+sudo -u ubuntu cp ./.vimrc $HOME
+sudo -u ubuntu mkdir  ~/.vim/plugged
 rm go1.12.linux-amd64.tar.gz
 echo "please run :PlugInstall and :GoInstallBinaries in vim and run vim_complete.sh"
